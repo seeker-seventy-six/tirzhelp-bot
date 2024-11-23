c3e93594cc62ed781b0b94d6a0fd627449a50b82
seeker-seventy-six <caden.valance@gmail.com>
2024-11-22 19:28:03 -0800

    update message fir lastcall

--- functions.py
+++ functions.py
@@ -45,9 +45,9 @@ def lastcall(update, bot_token):
     split_members = member_count - vial_donors
     
     # construct message
-    vial_donors_message = f"The group has elected to waive the vial donors testing portion, so the {vial_donors} are deducted from this calculation." if vial_donors else ""
+    vial_donors_message = f"NOTE: The group has elected to waive the vial donors testing payment portion, so those {vial_donors} members are accounted for in this calculation." if vial_donors else ""
     
-    lastcall_message = f"""Hi Researchers ❗📢❗ This is your final notice and last call for deciding whether you will be participating in this test. This test will be closing END OF TODAY! If you choose to stay in this group chat after today, you are committing to pay your portion of the testing costs and getting access to the test results. \nThe total testing cost is currently {test_cost} and with current number of users in the group is estimated to be ${test_cost/split_members:.2f} per researcher. {vial_donors_message} \n\n"""
+    lastcall_message = f"""Hi Researchers ❗📢❗ This is your final notice and last call for deciding if you will be participating in this test. This test will be closing END OF TODAY! If you choose to stay in this group chat after today, you are committing to pay your portion of the testing costs and getting access to the test results. \n\n**The total testing cost is currently ${test_cost} and with the current number of members in the group your payment portion is estimated to be ${test_cost/split_members:.2f}.** \n\n{vial_donors_message}"""
     
     return lastcall_message
 
