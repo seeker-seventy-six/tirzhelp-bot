9418f24742892ccf36757842eb05292fa5b9d141
seeker-seventy-six <caden.valance@gmail.com>
2024-12-03 00:19:56 -0800

    remove pin message action after messaging; can simply pin the message instead of the testing overhead

--- bot_webhook.py
+++ bot_webhook.py
@@ -63,13 +63,7 @@ def webhook():
             # Respond to the /lastcall command
             if text.startswith("/lastcall"):
                 lastcall_message = botfunc.lastcall(update, BOT_TOKEN)
-                response = send_message(chat_id, lastcall_message, message_thread_id)
-                if lastcall_message.startswith("Hello Researchers!"):
-                    try:
-                        lastcall_message_id = response['result']['message_id']
-                        pin_message(chat_id, lastcall_message_id)
-                    except:
-                        send_message(chat_id, "⚠️ If you would like me to pin this post, I will need Admin rights. Then rerun the command!", message_thread_id)
+                send_message(chat_id, lastcall_message, message_thread_id)
 
             # Respond to the /newbie command
             if text.startswith("/summarize"):
